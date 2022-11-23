2f843498b38841fba5f7e66e9082c76ad41b17ef
Stephanie <slee185@uncc.edu>
2022-11-23 12:22:33 -0500

    start polyline implementation 3rd attempt

--- app/src/main/java/com/example/group22_hw09/MainActivity.java
+++ app/src/main/java/com/example/group22_hw09/MainActivity.java
@@ -14,10 +14,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 import okhttp3.Call;
@@ -30,17 +36,12 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     private final OkHttpClient client = new OkHttpClient();
     LatLongResponse latLongResponse;
-    List<LatLng> latLngArrayList;
+    ArrayList<LatLng> latLngArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
 
         Request request = new Request.Builder()
                 .url("https://www.theappsdr.com/map/route")
@@ -56,38 +57,41 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                 Gson gson = new Gson();
                 latLongResponse = gson.fromJson(Objects.requireNonNull(response.body()).string(), LatLongResponse.class);
-                Log.d("TAG", "onResponse: "+ latLongResponse);
+                for(int i=0; i<latLongResponse.path.size(); i++){
+                    double latitude = latLongResponse.path.get(i).latitude;
+                    double longitude = latLongResponse.path.get(i).longitude;
 
-//                for(int i=0; i<latLongResponse.pathList.size(); i++){
-//                    LatLng point = latLongResponse.pathList.get(i);
-//                    double longitude = (double) latLongResponse.longitudes.get(i);
-//
-//                    LatLng point = new LatLng(latitude, longitude);
-//                    latLngArrayList.add(point);
-//                }
-//                Log.d("TAG", "onResponse: "+ latLngArrayList);
+                    LatLng point = new LatLng(latitude, longitude);
+                    Log.d("TAG", "onResponse: "+ point);
+                    latLngArrayList.add(point);
+                }
+                Log.d("TAG", "onResponse: "+ latLngArrayList);
             }
         });
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
 
-        if (latLngArrayList != null){
-            Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
-                    .clickable(true)
-                    .addAll(latLngArrayList));
-
-            googleMap.addMarker(new MarkerOptions()
-                    .position(latLngArrayList.get(0))
-                    .title("Start location"));
-
-            googleMap.addMarker(new MarkerOptions()
-                    .position(latLngArrayList.get(latLngArrayList.size()))
-                    .title("End location"));
-        }else{
-            Log.d("TAG", "onMapReady: ArrayList empty");
-        }
+//        if (latLngArrayList != null){
+//            Polyline polyline1 = googleMap.addPolyline(new PolylineOptions()
+//                    .clickable(true)
+//                    .addAll(latLngArrayList));
+//
+//            googleMap.addMarker(new MarkerOptions()
+//                    .position(latLngArrayList.get(0))
+//                    .title("Start location"));
+//
+//            googleMap.addMarker(new MarkerOptions()
+//                    .position(latLngArrayList.get(latLngArrayList.size()))
+//                    .title("End location"));
+//        }else{
+//            Log.d("TAG", "onMapReady: ArrayList empty");
+//        }
 
         }
     }
@@ -95,13 +99,13 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     class LatLongResponse {
 
     String title;
-    ArrayList<Path> pathList;
+    ArrayList<Path> path;
 
         @Override
         public String toString() {
             return "LatLongResponse{" +
                     "title='" + title + '\'' +
-                    ", pathList=" + pathList +
+                    ", path=" + path +
                     '}';
         }
     }
@@ -118,5 +122,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     ", longitude=" + longitude +
                     '}';
         }
+
     }
 
